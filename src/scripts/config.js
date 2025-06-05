eb85e4a3d18d6cd773934c121c7f18db36562d1e
duc <thienductang@gmail.com>
2025-06-05 21:08:40 +0700

    f

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,5 +1,5 @@
 const Config = {
-  ROSBRIDGE_SERVER_IP: "172.18.10.72",
+  ROSBRIDGE_SERVER_IP: "192.168.43.128",
   ROSBRIDGE_SERVER_PORT: "9090",
   RECONNECTION_TIMER: 3000,
   CMD_VEL_TOPIC: "/hoverboard_velocity_controller/cmd_vel",
