94ed7450e326568324401b5dbe5141b39c5b9485
duc <thienductang@gmail.com>
2025-06-20 12:23:25 +0700

    dynammic ssid and velocity params added

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,5 +1,5 @@
 const Config = {
-  ROSBRIDGE_SERVER_IP: "192.168.0.101",
+  ROSBRIDGE_SERVER_IP: "robot.local",
   ROSBRIDGE_SERVER_PORT: "9090",
   RECONNECTION_TIMER: 3000,
   CMD_VEL_TOPIC: "/hoverboard_velocity_controller/cmd_vel",
