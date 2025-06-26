55c47fd4a37bfc9821a99e10472329c4595060dd
duc <thienductang@gmail.com>
2025-06-26 10:10:04 +0700

    add service

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -2,7 +2,7 @@ const Config = {
   ROSBRIDGE_SERVER_IP: "192.168.0.102",
   ROSBRIDGE_SERVER_PORT: "9090",
   RECONNECTION_TIMER: 3000,
-  CMD_VEL_TOPIC: "/hoverboard_velocity_controller/cmd_vel",
+  CMD_VEL_TOPIC: "/cmd_vel_web",
   ODOM_TOPIC: "/odom",
   POSE_TOPIC: "/robot_pose",
   
