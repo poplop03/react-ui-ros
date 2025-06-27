d3b21a6f2d4e0d4713158b0a6c4c8503475e0f68
duc <thienductang@gmail.com>
2025-06-27 11:06:34 +0700

    dynamic speed

--- src/components/Teleoperation.jsx
+++ src/components/Teleoperation.jsx
@@ -8,13 +8,12 @@ class Teleoperation extends Component {
     this.state = {
       ros: null,
       connected: false,
-      linearSpeed: 0.5,
-      angularSpeed: 1.0,
+      linearSpeed: 0.4,    // meters per second
+      angularSpeed: 0.2,   // radians per second
     };
 
-    this.stopTimeout = null;
     this.handleMove = this.handleMove.bind(this);
-    this.handleStop = this.handleStop.bind(this);
+    // this.handleStop = this.handleStop.bind(this);
   }
 
   componentDidMount() {
@@ -62,36 +61,28 @@ class Teleoperation extends Component {
     const { linearSpeed, angularSpeed } = this.state;
 
     const twist = new window.ROSLIB.Message({
-      linear: { x: (event.y / 87.5) * linearSpeed, y: 0, z: 0 },
-      angular: { x: 0, y: 0, z: (-event.x / 87.5) * angularSpeed },
+      linear: {
+        x: (event.y / 87.5) * linearSpeed,
+        y: 0,
+        z: 0,
+      },
+      angular: {
+        x: 0,
+        y: 0,
+        z: (-event.x / 87.5) * angularSpeed,
+      },
     });
 
     this.cmd_vel.publish(twist);
-
-    // Reset and start new stop timeout
-    clearTimeout(this.stopTimeout);
-    this.stopTimeout = setTimeout(this.handleStop, 300);
   }
 
-  handleStop() {
-    if (!this.cmd_vel) return;
-
-    console.log("Sending stop command");
-    const stopTwist = new window.ROSLIB.Message({
-      linear: { x: 0, y: 0, z: 0 },
-      angular: { x: 0, y: 0, z: 0 },
-    });
-
-    this.cmd_vel.publish(stopTwist);
-  }
 
   render() {
     return (
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-end",
+          justifyContent: "flex-end",
           padding: "10px",
         }}
       >
@@ -102,39 +93,11 @@ class Teleoperation extends Component {
           move={this.handleMove}
           stop={this.handleStop}
         />
-
-        <div style={{ marginTop: "20px", textAlign: "right" }}>
-          <label>
-            Linear Speed:
-            <input
-              type="range"
-              min="0"
-              max="2"
-              step="0.1"
-              value={this.state.linearSpeed}
-              onChange={(e) => this.setState({ linearSpeed: parseFloat(e.target.value) })}
-              style={{ width: "150px", marginLeft: "10px" }}
-            />
-            <span style={{ marginLeft: "10px" }}>{this.state.linearSpeed.toFixed(1)} m/s</span>
-          </label>
-          <br />
-          <label style={{ marginTop: "10px" }}>
-            Angular Speed:
-            <input
-              type="range"
-              min="0"
-              max="5"
-              step="0.1"
-              value={this.state.angularSpeed}
-              onChange={(e) => this.setState({ angularSpeed: parseFloat(e.target.value) })}
-              style={{ width: "150px", marginLeft: "10px" }}
-            />
-            <span style={{ marginLeft: "10px" }}>{this.state.angularSpeed.toFixed(1)} rad/s</span>
-          </label>
-        </div>
       </div>
     );
   }
 }
 
+
+// this nigga workingd
 export default Teleoperation;
