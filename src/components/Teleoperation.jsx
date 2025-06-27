5e99de73f4f0b291ba8f7e81ed48981d7acc1d00
duc <thienductang@gmail.com>
2025-06-27 11:08:59 +0700

    dynamic speed

--- src/components/Teleoperation.jsx
+++ src/components/Teleoperation.jsx
@@ -8,12 +8,13 @@ class Teleoperation extends Component {
     this.state = {
       ros: null,
       connected: false,
-      linearSpeed: 0.4,    // meters per second
-      angularSpeed: 0.2,   // radians per second
+      linearSpeed: 0.5,
+      angularSpeed: 1.0,
     };
 
+    this.stopTimeout = null;
     this.handleMove = this.handleMove.bind(this);
-    // this.handleStop = this.handleStop.bind(this);
+    this.handleStop = this.handleStop.bind(this);
   }
 
   componentDidMount() {
@@ -61,48 +62,36 @@ class Teleoperation extends Component {
     const { linearSpeed, angularSpeed } = this.state;
 
     const twist = new window.ROSLIB.Message({
-      linear: {
-        x: (event.y / 87.5) * linearSpeed,
-        y: 0,
-        z: 0,
-      },
-      angular: {
-        x: 0,
-        y: 0,
-        z: (-event.x / 87.5) * angularSpeed,
-      },
+      linear: { x: (event.y / 87.5) * linearSpeed, y: 0, z: 0 },
+      angular: { x: 0, y: 0, z: (-event.x / 87.5) * angularSpeed },
     });
 
     this.cmd_vel.publish(twist);
+
+    // Reset and start new stop timeout
+    clearTimeout(this.stopTimeout);
+    this.stopTimeout = setTimeout(this.handleStop, 300);
   }
 
-      handleStop(){
-        var cmd_vel = new window.ROSLIB.Topic({
-            ros: this.state.ros,
-            name: Config.CMD_VEL_TOPIC,
-            messageType: "geometry_msgs/Twist",
-        });
-        var twist = new window.ROSLIB.Message({
-            linear : {
-                x: 0,
-                y: 0,
-                z: 0,
-            },
-            angular: {
-                x: 0,
-                y: 0,
-                z: 0,
-            }
-        });
-        cmd_vel.publish(twist);
-    }
+  handleStop() {
+    if (!this.cmd_vel) return;
+
+    console.log("Sending stop command");
+    const stopTwist = new window.ROSLIB.Message({
+      linear: { x: 0, y: 0, z: 0 },
+      angular: { x: 0, y: 0, z: 0 },
+    });
+
+    this.cmd_vel.publish(stopTwist);
+  }
 
   render() {
     return (
       <div
         style={{
           display: "flex",
-          justifyContent: "flex-end",
+          flexDirection: "column",
+          alignItems: "flex-end",
           padding: "10px",
         }}
       >
@@ -113,11 +102,39 @@ class Teleoperation extends Component {
           move={this.handleMove}
           stop={this.handleStop}
         />
+
+        <div style={{ marginTop: "20px", textAlign: "right" }}>
+          <label>
+            Linear Speed:
+            <input
+              type="range"
+              min="0"
+              max="2"
+              step="0.1"
+              value={this.state.linearSpeed}
+              onChange={(e) => this.setState({ linearSpeed: parseFloat(e.target.value) })}
+              style={{ width: "150px", marginLeft: "10px" }}
+            />
+            <span style={{ marginLeft: "10px" }}>{this.state.linearSpeed.toFixed(1)} m/s</span>
+          </label>
+          <br />
+          <label style={{ marginTop: "10px" }}>
+            Angular Speed:
+            <input
+              type="range"
+              min="0"
+              max="5"
+              step="0.1"
+              value={this.state.angularSpeed}
+              onChange={(e) => this.setState({ angularSpeed: parseFloat(e.target.value) })}
+              style={{ width: "150px", marginLeft: "10px" }}
+            />
+            <span style={{ marginLeft: "10px" }}>{this.state.angularSpeed.toFixed(1)} rad/s</span>
+          </label>
+        </div>
       </div>
     );
   }
 }
 
-
-// this nigga workingd
 export default Teleoperation;
