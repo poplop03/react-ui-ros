f4f09708e3129a0772d1c5fdca6f61081d0f7e7c
duc <thienductang@gmail.com>
2025-07-09 14:13:23 +0700

    dif

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -39,7 +39,7 @@ class Map extends Component {
   componentDidMount() {
     this.init_connection();
     console.log("Map: componentDidMount" + this.state.ros);
-    this.view_map();   //comment this to off map
+    //this.view_map();   //comment this to off map
   }
 
   view_map() {
